eb363c7b1645ec520d22b10b2838a8c1a4e3e320
Alyona Tagan <p.lyolik@mail.ru>
2021-07-27 15:31:35 +0400

    add slider

--- src/js/pages/Slider.js
+++ src/js/pages/Slider.js
@@ -2,25 +2,84 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Api } from '../Api';
 import { addImages } from '../redux/actions';
+import { Link } from 'react-router-dom';
+import '../../scss/components/slider.scss';
 
 class Slider extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      imgIndex: 0,
+      images: [],
+      isLocal: true
+    }
+  }
 
-  loadImages = () => {
-    Api.getImages().then(arr => {
-      this.props.addImages(arr)
+  importAll(r) {
+    console.log(r.keys())
+    return r.keys().map(item => r(item).default)
+  }
+
+  loadImagesLocal = () => {
+    const images = this.importAll(require.context('../../img', false, /\.(png|jpe?g|svg)$/))
+    this.setState({
+      images: images,
+      imgIndex: 0,
+      isLocal: false
     })
   }
 
+  loadImagesServer = () => {
+    Api.getImages().then(arr => {
+      this.props.addImages(arr)
+      this.setState({
+        images: this.props.images,
+        imgIndex: 0,
+        isLocal: true
+      })
+    })
+  }
+
+  handleSwitchLoadingPlace = (e) => {
+    this.state.isLocal ? this.loadImagesLocal() : this.loadImagesServer()
+  }
+
+  handleToLeftClick = (e) => {
+    let i = this.state.imgIndex
+    i = i - 1
+    if (i < 0) {
+      i = this.state.images.length - 1
+    }
+    this.setState({ imgIndex: i })
+  }
+
+  handleToRightClick = (e) => {
+    let i = this.state.imgIndex
+    i = i + 1
+    if (i >= this.state.images.length) {
+      i = 0
+    }
+    this.setState({ imgIndex: i })
+  }
+
   componentDidMount = () => {
-    this.loadImages()
+    this.loadImagesServer()
   }
 
   render = () => {
     return (
-      <div>
-        {this.props.images.map((img, i) => ((
-          <img alt='img' key={i} src={img}></img>
-        )))}
+      <div className="slider_block">
+        <div className="slider">
+          <button onClick={this.handleToLeftClick} className="btn-switch"> Left </button>
+          <div className="slider_img">
+            <img className="img" alt='img' src={this.state.images[this.state.imgIndex]}></img>
+          </div>
+          <button onClick={this.handleToRightClick} className="btn-switch"> Right </button>
+        </div>
+        <button onClick={this.handleSwitchLoadingPlace} className="btn">  Switch to {this.state.isLocal ? 'remote' : 'local'}</button>
+        <Link to="/main">
+          <button className="btn"> Back to main</button>
+        </Link>
       </div>
     )
   }
